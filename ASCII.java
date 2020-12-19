69eef6fd211c7c2a5e2c5fc4587e534c657186c2
Nil Bhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-12-19 10:46:58 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,18 @@
+package javacourse;
+
+//  4.Find ASCII value of a character
+public class ASCII
+{
+    public static void main(String[] args)
+    {
+      char ch = 'a';
+        int ascii = ch;
+        
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    
+                
+    }
+}
