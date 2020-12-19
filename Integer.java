5a089a2b96c840551620c473b3391463bbe110e1
Nil Bhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-12-19 10:32:20 +0530

    Create Integer.java

---
+++ Integer.java
@@ -0,0 +1,20 @@
+package javacourse;
+
+import java.util.Scanner;
+
+/* 1.Integer(Entered by  User)*/
+public class JavaCourse 
+{
+
+    
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner x;
+        x= new Scanner(System.in); 
+        System.out.println("Enter n= ");
+        n=x.nextInt();
+        System.out.println("n= "+n);
+    }
+    
+}
