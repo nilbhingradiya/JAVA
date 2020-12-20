5cb1b1eaad208ed607badb848db4e0550103ce1a
Nil Bhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-12-20 09:36:34 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,41 @@
+package javacourse;
+
+import java.util.Scanner;
+
+
+public class leapyear 
+{
+    public static void main(String[] args)
+    {
+        int year;
+        Scanner x;
+        x=new Scanner(System.in);
+        System.out.println("Enter year=");
+        year=x.nextInt();
+        boolean leap = false;
+        if (year % 4 == 0) {
+
+     
+        if (year % 100 == 0) 
+        {
+        if (year % 400 == 0)
+          leap = true;
+        else
+          leap = false;
+        }
+      
+     
+         else
+        leap = true;
+         }
+    
+        else
+        leap = false;
+
+        if (leap)
+        System.out.println(year + " is a leap year.");
+        else
+        System.out.println(year + " is not a leap year.");
+  
+    }
+}
